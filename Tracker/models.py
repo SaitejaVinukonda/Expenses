7c963fdc23387734e161fbc252537cb7fc0daf6a
saiteja <saitejavinukond684@gmail.com>
2025-03-07 08:39:53 +0530

    added income and expenses logic

--- Tracker/models.py
+++ Tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class currentbalance(models.Model):
+    current_balance=models.FloatField(default=0)
+class TrackingHistory(models.Model):
+    current_balance=models.ForeignKey(currentbalance,on_delete=models.CASCADE)
+    amount=models.FloatField()
+    expense_type=models.CharField(choices=(('credit','credit'),('debit','debit')),max_length=200)
+    description=models.CharField(max_length=200)
+    created_at=models.DateTimeField(auto_now=True)
+    created_at=models.DateTimeField(auto_now=True)
--- Tracker/views.py
+++ Tracker/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import TrackingHistory, currentbalance
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method ==  "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        current_balance,_=currentbalance.objects.get_or_create(id=1)
+        expense_type="credit"
+        if float(amount) < 0:
+            expense_type="debit"
+        tracking_history = TrackingHistory.objects.create(
+            amount=amount,
+            expense_type=expense_type,
+            current_balance=current_balance,
+            description=description)
+        current_balance.current_balance += float(tracking_history.amount) 
+        current_balance.save()        
+        print(description,amount)
+        return redirect('/')
+    current_balance,_=currentbalance.objects.get_or_create(id=1)
+    income=0
+    expense=0
+    for tracking_history in TrackingHistory.objects.all():
+        if tracking_history.expense_type == 'credit':
+            income+=tracking_history.amount
+        else:
+            expense+=tracking_history.amount
+
+    
+    context = {'income':income,'expense':expense,
+    'transactions':TrackingHistory.objects.all(),
+    'current_balance':current_balance
+    }                 
+    return render(request,'index.html', context)
+
+
